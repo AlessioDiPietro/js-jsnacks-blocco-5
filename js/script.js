4d4b024ac06b7bc1d208b0a731c4113055a0ff75
AlessioDPT <alessio.recos@gmail.com>
2021-06-15 16:16:08 +0200

    MOD-es_1 stampa html

--- js/script.js
+++ js/script.js
@@ -25,13 +25,57 @@ var  minPeso = negozioBici[0];
 
 for (var i = 1; i < negozioBici.length; i ++) {
     if (negozioBici[i].peso < minPeso.peso){
-        pesoMin = negozioBici[i];
+        minPeso = negozioBici[i];
     }
 }
 
-document.getElementById("es_1").innerHTML= "la bici con il minor peso è: " + pesoMin;
+document.getElementById("es_1").innerHTML = "la bici con il minor peso è: " + minPeso;
 
 
+//! es_2
 // 2. Crea un array di 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza.
 // Calcola quanto pesano tutte le zucchine.
+
+
+var pesoTot = 0;
+
+
+var bancarella = [
+    {
+        varieta : "naturella",
+        peso : 10,
+        lunghezza : 2.5,
+    },
+
+    {
+        varieta : "paesana",
+        peso : 15,
+        lunghezza : 1.5,
+    },
+
+    {
+        varieta : "bio",
+        peso : 18,
+        lunghezza : 3,
+    },
+
+    {
+        varieta : "fresca",
+        peso : 12,
+        lunghezza : 3.5,
+    }
+];
+
+
+for (var i = 0; i < bancarella.length; i++){
+    
+    console.log(bancarella[i]);
+    
+    pesoTot += bancarella[i].peso;
+}
+
+document.getElementById("es_2").innerHTML= "il totale del peso di tutte le zucchine è pari a " + pesoTot;
+
+
+
 // 3. Crea un array di 10 oggetti che rappresentano una zucchina. Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine
